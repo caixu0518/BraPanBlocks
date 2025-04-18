22a48d34d36c6cd86aa7c1b5704accc12f0d0ce5
Xu Cai <33691227+caixu0518@users.noreply.github.com>
2025-04-18 08:40:10 +0800

    Delete PopulationStructureAnalysis/Treemix directory

--- PopulationStructureAnalysis/Treemix/generate_treemix.cmds.sh
+++
@@ -1,9 +0,0 @@
-for i in {0..10}
-    do
-    for j in {1..10}
-        do
-        echo   treemix -i   ECD04.snps.maf0.05.fm.sub.clean.pruned.input_for_treemix.frq.txt.gz    -root Rapini -k 500 -m ${i} -bootstrap -o migration_${i}_bootstrap_${j}
-    done
-done
-
-
